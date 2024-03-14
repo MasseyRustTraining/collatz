0d137c9772c4fda858716753db284a4baebbe3f7
Bart Massey <bart.massey@gmail.com>
2024-03-14 11:46:10 -0700

    added doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//! [Collatz Sequence
+//! exercise](https://google.github.io/comprehensive-rust/control-flow-basics/exercise.html)
+//! from Comprehensive Rust.
+
+/// Returns the length of the [Collatz
+/// Sequence](https://en.wikipedia.org/wiki/Collatz_conjecture)
+/// starting at `x`.
 fn collatz_length(mut x: i32) -> u32 {
     let mut n = 1;
     while x != 1 {
