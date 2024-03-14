81ad6a1d5378870aca57fb55abfee81375a0da6e
Bart Massey <bart.massey@gmail.com>
2024-03-14 13:13:44 -0700

    split into binary and library crates

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,4 @@
-//! [Collatz Sequence
-//! exercise](https://google.github.io/comprehensive-rust/control-flow-basics/exercise.html)
-//! from Comprehensive Rust.
-
-/// Returns the number of steps (length - 1) of the [Collatz
-/// Sequence](https://en.wikipedia.org/wiki/Collatz_conjecture)
-/// starting at `x`.
-///
-/// # Examples
-///
-/// ```
-/// assert!(collatz_length(3) == 8);
-/// ```
-fn collatz_length(mut x: u64) -> usize {
-    let mut n = 0;
-    while x != 1 {
-        match x & 1 == 1 {
-            true => {
-                // x is odd.
-                x = 3 * x + 1;
-            }
-            false => {
-                // x is even.
-                x /= 2;
-            }
-        }
-        n += 1;
-    }
-    n
-}
-
-#[test]
-fn test_collatz_length() {
-    assert!(collatz_length(1) == 0);
-    assert!(collatz_length(3) == 7);
-    // From https://en.wikipedia.org/wiki/Collatz_conjecture
-    assert!(collatz_length(670_617_279) == 986);
-    assert!(collatz_length(989_345_275_647) == 1348);
-}
+use collatz::collatz_length;
 
 fn main() {
     let start = std::env::args().nth(1).unwrap().parse().unwrap();
