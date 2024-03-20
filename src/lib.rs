d6734caaa2da0fa40a962fdedc64146511682169
Bart Massey <bart.massey@gmail.com>
2024-03-19 20:23:59 -0700

    check for overflow in 3n+1
    closes #2

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@
 //! exercise](https://google.github.io/comprehensive-rust/control-flow-basics/exercise.html)
 //! from Comprehensive Rust.
 
-/// Returns [Some] number of steps (length - 1) of the [Collatz
+/// Returns [Some] number of steps (length - 1) of the
+/// [Collatz
 /// Sequence](https://en.wikipedia.org/wiki/Collatz_conjecture)
-/// starting at `x`; if `x` is 0, instead return [None].
+/// starting at `x`; if `x` is 0 or the calculation
+/// overflows, instead return [None].
 ///
 /// # Examples
 ///
@@ -21,7 +23,7 @@ pub fn collatz_length(mut x: u64) -> Option<usize> {
         match x & 1 == 1 {
             true => {
                 // x is odd.
-                x = 3 * x + 1;
+                x = x.checked_mul(3)?.checked_add(1)?;
             }
             false => {
                 // x is even.
@@ -43,11 +45,13 @@ mod test {
 
     #[test]
     fn test_collatz_length() {
-        assert!(collatz_length(0) == None);
+        assert!(collatz_length(0).is_none());
         test_1(1, 0);
         assert!(collatz_length(3).unwrap() == 7);
         // From https://en.wikipedia.org/wiki/Collatz_conjecture
         assert!(collatz_length(670_617_279).unwrap() == 986);
         assert!(collatz_length(989_345_275_647).unwrap() == 1348);
+        // Overflows.
+        assert!(collatz_length(0xffff_ffff_ffff_ffff).is_none());
     }
 }
